ea5cab26e59a88cc9fc0dfab31f31bd28c80e638
Connor Bunch <connorbunch@outlook.com>
2024-03-25 17:41:45 -0600

    added launch.json and auto npm install

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return <>
-        <h1>Welcome to my blog! 😀</h1>
+        <h1>Welcome to my blog!!!!!! 😀</h1>
         <h2>It's a work in progress, but I promise something neat is coming!</h2>
     </>
 }
